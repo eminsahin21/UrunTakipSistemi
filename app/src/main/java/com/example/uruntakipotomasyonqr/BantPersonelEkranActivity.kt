34d20d3eab754313f7feac455852b6a455111bbc
Mehmet Emin <mehmeteminsahin34@gmail.com>
2023-07-31 12:02:36 +0300

    ekran duzenlendi

---
+++ app/src/main/java/com/example/uruntakipotomasyonqr/BantPersonelEkranActivity.kt
@@ -0,0 +1,11 @@
+package com.example.uruntakipotomasyonqr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BantPersonelEkranActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bant_personel_ekran)
+    }
+}
